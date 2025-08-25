32ca3fb63d1c383c5b58c5706e2e340d4f644ab8
nicolle-lo-ve <135842806+nicolle-lo-ve@users.noreply.github.com>
2025-08-25 11:57:41 -0500

    Creación de main
    Archivo principal, acceso a los demás archivos a través de presentación de menú

---
+++ P1_INTRO_KOTLIN/src_p1/Main.kt
@@ -0,0 +1,19 @@
+fun main() {
+  var opcion: int
+  do {
+    println("\n ------- MENÚ PRINCIPAL -------")
+    println("1. EVALAUCIÓN DE EMPLEADOS")
+    println("2. PIEDRA, PAPEL, TIJERA ")
+    println("3. CALCULADORA ELEMENTAL")
+    println("4. ADIVINA EL NÚMERO")
+    println("5. SALIR")
+    println("Seleccione una opción (1-5): ")
+
+    opcion = readln()
+
+    when (opcion) {
+      5 -> println("¡Gracias por usar el programa!")
+      else -> println("Opción en desarrollo...")
+    }
+  } while (opcion =! 5)
+}
