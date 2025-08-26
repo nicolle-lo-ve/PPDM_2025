ed3120a1fcdf61c48546e4cad044f28f5d400b63
nicolle-lo-ve <135842806+nicolle-lo-ve@users.noreply.github.com>
2025-08-26 00:31:58 -0500

    Implementacion de Random
    Se agregaron las tres opciones disponibles del juego, además del uso de la biblioteca random para hacer la elección aleatoria de la computadora.

--- P1_INTRO_KOTLIN/src_p1/piedra_papel_tijera.kt
+++ P1_INTRO_KOTLIN/src_p1/piedra_papel_tijera.kt
@@ -1,3 +1,11 @@
+import kotlin.random.Random
+
 fun ejercicioPiedraPapelTijera(){
   println ("\n ------ PIEDRA, PAPEL, TIJERA ------ ")
+
+  // Opciones Disponibles
+  val opciones = listOf("piedra", "papel", "tijera")
+
+  // Elección aleatoria de la computadora
+  val eleccionComputadora = opciones[Random.nextInt(from = 0, until = 3)]
 }
