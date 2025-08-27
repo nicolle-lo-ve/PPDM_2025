a48bbbed3715bb523f596ca74dc570ddaa092ef6
nicolle-lo-ve <135842806+nicolle-lo-ve@users.noreply.github.com>
2025-08-27 14:01:30 -0500

    MENU
    Se creó un menú con 5 opciones, cuatro de ellas hacen operaciones y una de ellas sale del ejercicio

--- P1_INTRO_KOTLIN/src_p1/calculadora_elemental.kt
+++ P1_INTRO_KOTLIN/src_p1/calculadora_elemental.kt
@@ -1,2 +1,15 @@
 fun ejercicioCalculadoraElemental() {
+  var opcion: Int
+  do {
+    println("\n ------ CALCULADORA ELEMENTAL ------")
+    println("1. Suma (+)")
+    println("2. Resta (-)")
+    println("3. Multiplicación (×)")
+    println("4. División (÷)")
+    println("5. Volver al menú principal")
+    println("\n Seleccione una operación (1-5): ")
+
+    opcion = readln().toInt()
+
+  } while (opcion != 5)
 }
