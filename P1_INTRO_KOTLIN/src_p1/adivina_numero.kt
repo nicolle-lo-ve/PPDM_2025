99c90d3a382936c19fa7e10809215d849c4f3c7f
nicolle-lo-ve <135842806+nicolle-lo-ve@users.noreply.github.com>
2025-09-04 17:40:09 -0500

    Generación de número
    Con la librería random se selecciona el número a adivinar y se guarda en la variable numeroAdivinar

--- P1_INTRO_KOTLIN/src_p1/adivina_numero.kt
+++ P1_INTRO_KOTLIN/src_p1/adivina_numero.kt
@@ -1,3 +1,13 @@
 import kotlin.random.Random
 fun ejercicioAdivinaNumero() {
+println("\n ------ ADIVINA EL NÚMERO ------")
+println(" Tienes 5 intentos para adivinar un número entre 1 y 30 ")
+
+// Generar número aleatorio
+val numeroAdivinar = Random.nextInt(1, 31)
+var intentos = 5
+var adivinado = false
+  
+// Mostrar resultado final
+println("\n ------ RESULTADO FINAL ------")
 }
