9d983f512f8d4f3d52476e04a0f9e249cf780300
nicolle-lo-ve <135842806+nicolle-lo-ve@users.noreply.github.com>
2025-09-04 17:57:45 -0500

    Resultados
    Mensajes finales sobre los resultados del usuario

--- P1_INTRO_KOTLIN/src_p1/adivina_numero.kt
+++ P1_INTRO_KOTLIN/src_p1/adivina_numero.kt
@@ -1,39 +1,47 @@
 import kotlin.random.Random
 fun ejercicioAdivinaNumero() {
-println("\n ------ ADIVINA EL NÚMERO ------")
-println(" Tienes 5 intentos para adivinar un número entre 1 y 30 ")
-
-// Generar número aleatorio
-val numeroAdivinar = Random.nextInt(1, 31)
-var intentos = 5
-var adivinado = false
-
-// Ciclo de intentos
-while (intentos > 0 && !adivinado) {
-  println("Intentos restantes: $intentos ")
-  print("Ingresa tu respuesta: ")
-  val respuesta = readln().toInt()
-
-  // Validar respuesta
-  if (respuesta in 1<= ..<= 30) {
-      println("Por favor, ingresa un número válido entre 1 y 30.")
-      continue
+  println("\n ------ ADIVINA EL NÚMERO ------")
+  println(" Tienes 5 intentos para adivinar un número entre 1 y 30 ")
+  
+  // Generar número aleatorio
+  val numeroAdivinar = Random.nextInt(1, 31)
+  var intentos = 5
+  var adivinado = false
+  
+  // Ciclo de intentos
+  while (intentos > 0 && !adivinado) {
+    println("Intentos restantes: $intentos ")
+    print("Ingresa tu respuesta: ")
+    val respuesta = readln().toInt()
+  
+    // Validar respuesta
+    if (respuesta in 1<= ..<= 30) {
+        println("Por favor, ingresa un número válido entre 1 y 30.")
+        continue
+    }
+    
+    // Verificar respuesta con pista
+    when {
+        respuesta == numeroAdivinar -> {
+            adivinado = true
+            println("¡Felicidades! Adivinaste el número $numeroAdivinar")
+        }
+        respuesta < numeroAdivinar -> println("El número es mayor que $respuesta")
+        else -> println("El número es menor que $respuesta")
+    }
+    
+    intentos--
   }
   
-  // Verificar respuesta con pista
-  when {
-      respuesta == numeroAdivinar -> {
-          adivinado = true
-          println("¡Felicidades! Adivinaste el número $numeroAdivinar")
-      }
-      respuesta < numeroAdivinar -> println("El número es mayor que $respuesta")
-      else -> println("El número es menor que $respuesta")
-  }
+  // Mostrar resultado final
+  println("\n ------ RESULTADO FINAL ------")
   
-  intentos--
-}
+  if (adivinado) {
+      println("¡Ganaste!")
+      println("Adivinaste en ${5 - intentos} intento(s)")
+  } else {
+      println("No adivinaste")
+      println("El número era: $numeroAdivinar")
+  }
 
-
-// Mostrar resultado final
-println("\n ------ RESULTADO FINAL ------")
 }
