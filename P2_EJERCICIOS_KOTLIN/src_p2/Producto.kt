6f2b22eb0a78e9330f4499ae87c16f766627a17f
nicolle-lo-ve <135842806+nicolle-lo-ve@users.noreply.github.com>
2025-09-04 19:54:51 -0500

    Implementar getters y setters para precio
    Se añade métodos getPrecio() y setPrecio() con validación para valores no negativos

--- P2_EJERCICIOS_KOTLIN/src_p2/Producto.kt
+++ P2_EJERCICIOS_KOTLIN/src_p2/Producto.kt
@@ -1,4 +1,14 @@
 class Producto {
     private var precio: Double = 0.0
     private var descuento: Double = 0.0
+
+    fun setPrecio(nuevoPrecio: Double) {
+        if (nuevoPrecio >= 0) {
+            precio = nuevoPrecio
+        } else {
+            println("Error: El precio no puede ser negativo")
+        }
+    }
+
+    fun getPrecio(): Double = precio
 }
